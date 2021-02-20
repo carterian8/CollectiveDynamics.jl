33dfa0b07762e7da8839ffde240ffb958e1c1721
Ian Carter <carterian8@gmail.com>
2021-02-20 08:36:13 -0500

    Adding modified simulation driver for 2d vicsek

--- examples/vicsek_2D_simulation.jl
+++ examples/vicsek_2D_simulation.jl
@@ -7,39 +7,46 @@
 ### ============ INCLUDE PACKAGES ============ ###
 
 using CollectiveDynamics.SVM2D
+using Plots
 
 ### ============ SYSTEM'S PARAMETERS ============ ###
 
 N   = parse(Int, ARGS[1]) # number of particles
 ρ   = parse(Float64, ARGS[2]) # density
 η   = parse(Float64, ARGS[3]) # noise intensity
-T   = parse(Int, ARGS[4]) # integration time steps
-rep = parse(Int, ARGS[5]) # ensemble index
+ω   = parse(Float64, ARGS[4]) # maximal angular velocity
+φ   = parse(Float64, ARGS[5]) # particle field of view
+T   = parse(Int, ARGS[6]) # integration time steps
+rep = parse(Int, ARGS[7]) # ensemble index
+viz = true # visualize steps
 
 ### =============== ### =============== ###
 
-v0 = 1.0 # particle's speed
+v0 = 0.2 # 1.0 # particle's speed
 dt = 1.0 # integration timestep
 
 ### =============== ### =============== ###
 
 M = 10 # number of boxes per dimension
 
-l = 0.5 # interaction range is double the distance each particle moves in one time step
+# l = 0.5 # interaction range is double the distance each particle moves in one time step
 #l = 0.1 # interaction range is ten times the distance each particle moves in one time step
 
-r0 = (v0 * dt) / l # interaction range
+# r0 = (v0 * dt) / l # interaction range
+ratio = 1.03
+# r0 = v0 / (ratio * ω) # interaction range
+r0 = 1.0
 
-L  = sqrt(N / ρ) # size of box
+L = sqrt(N / ρ) # size of box
 
 ### =============== ### =============== ###
 
-cell_size = step(linspace(0., L , M))
+cell_size = step(range(0., stop=L , length=M))
 
 ### ============ SYSTEM'S INITIALIZATION ============ ###
 
 box = Box(L, M) # suqare box of size L and M partitions per dimension
-flock = Flock(N, L, dt, v0, r0, η) # flock initialization
+flock = Flock(N, L, dt, v0, r0, η, ω, φ) # flock initialization
 
 ### ============== ### ============== ###
 
@@ -49,6 +56,17 @@ pos_file = open(output_path * "/pos_$(rep).dat", "w+")
 vel_file = open(output_path * "/vel_$(rep).dat", "w+")
 
 times = [convert(Int, exp10(i)) for i in 0:T]
+
+plt = !viz ? nothing : quiver(
+    [ flock.pos[i][1] for i in 1:N ],
+    [ flock.pos[i][2] for i in 1:N ],
+    quiver=(
+        [ flock.vel[i][1] for i in 1:N ],
+        [ flock.vel[i][2] for i in 1:N ]
+    ),
+    legend=false
+)
+savefig(plt, output_path * "/step0.png")
 
 ### ============ TIME EVOLUTION ============ ###
 
@@ -62,6 +80,18 @@ for i in 1:(length(times) - 1)
 
             if t % times[i] == 0 || t % times[i-1] == 0
                 println("//////// ", t)
+                if viz
+                    plt = quiver(
+                        [ flock.pos[i][1] for i in 1:N ],
+                        [ flock.pos[i][2] for i in 1:N ],
+                        quiver=(
+                            [ flock.vel[i][1] for i in 1:N ],
+                            [ flock.vel[i][2] for i in 1:N ]
+                        ),
+                        legend=false
+                    )
+                    savefig(plt, output_path * "/step$(t).png")
+                end
                 write(pos_file, vcat(flock.pos...))
                 write(vel_file, vcat(flock.vel...))
             end
@@ -75,6 +105,18 @@ for i in 1:(length(times) - 1)
 
             if t % times[i] == 0
                 println("//////// ", t)
+                if viz
+                    plt = quiver(
+                        [ flock.pos[i][1] for i in 1:N ],
+                        [ flock.pos[i][2] for i in 1:N ],
+                        quiver=(
+                            [ flock.vel[i][1] for i in 1:N ],
+                            [ flock.vel[i][2] for i in 1:N ]
+                        ),
+                        legend=false
+                    )
+                    savefig(plt, output_path * "/step$(t).png")
+                end
                 write(pos_file, vcat(flock.pos...))
                 write(vel_file, vcat(flock.vel...))
             end
